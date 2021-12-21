d147764c2cb8cfb9094c1aa20688e1ee9931726a
ivanbaug <ivsitioinfo@gmail.com>
2021-12-20 21:59:49 -0500

    add dynamic product page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Footer from "./components/Footer";
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
-
-
 function App() {
   return (
     <BrowserRouter>
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,7 +1,67 @@
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap"
+import { Link, useParams } from "react-router-dom"
+import Rating from "../components/Rating"
+// import Review from "../components/Review"
+
+import products from "../products"
+
 const ProductScreen = () => {
+  const params = useParams()
+  const product = products.find((p) => p._id === params.id)
   return (
     <div>
-      Product
+      <Link to='/' className="btn btn-light my-3">Go Back</Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price: ${product.price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Description: {product.description}
+            </ListGroup.Item>
+
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price: </Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status: </Col>
+                  <Col>
+                    {product.countInStock >= 1 ? 'In Stock' : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div className="d-grid gap-2">
+                  <Button type="button" disabled={product.countInStock === 0} >
+                    {product.countInStock >= 1 ? 'Add to cart' : 'Out of Stock'}
+                  </Button>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
